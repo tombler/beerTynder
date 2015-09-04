6d269edd0173257c15d5594fc8d09e16366b1cdd
TGriffey <tgriffey9389@gmail.com>
2015-09-04 13:39:03 -0500

    Added URL routing in app.js

--- www/js/app.js
+++ www/js/app.js
@@ -40,46 +40,36 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
+  .state('tab.landing', {
+    url: '/user/home', //url: '/:userId/home',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
+      'tab-home': {
+        templateUrl: 'templates/landing.html',
+        controller: 'LandingCtrl'
       }
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.explore', {
+      url: '/user/explore', // url: '/:userId/explore',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+        'tab-explore': {
+          templateUrl: 'templates/explore.html',
+          controller: 'ExploreCtrl'
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
+    .state('tab.wishlist', {
+      url: '/user/wishlist', // url: '/:userId/wishlist'
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'tab-wishlist': {
+          templateUrl: 'templates/wishlist.html',
+          controller: 'WishlistCtrl'
         }
       }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/user/home'); // 'tab/:userId/home'
 
 });
